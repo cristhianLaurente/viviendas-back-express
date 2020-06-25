1ad366813b0609cd462dba2afa9d4f22f5b1d335
cristhianLaurente <claurente.1999@gmail.com>
2020-06-24 21:07:38 -0500

    property by id!

--- src/controllers/property.ts
+++ src/controllers/property.ts
@@ -21,6 +21,40 @@ export const getProperties = (req: Request, res: Response) => {
             })
 }
 
+export const getPropertyById = (req: Request, res: Response) => {
+    let {id} = req.params
+
+    Property.findById(id)
+            .exec((err, propertyDB) => {
+                if(err) {
+                    return res.status(500).json({
+                        success: false,
+                        error: {
+                            title: 'database error',
+                            message: err
+                        }
+                    })
+                }
+                if(!propertyDB) {
+                    return res.status(400).json({
+                        success: false,
+                        error: {
+                            title: 'Bad Request',
+                            message: 'id is invalid'
+                        }
+                    })
+                }
+
+                res.status(200).json({
+                    success: true,
+                    data: propertyDB
+                })
+
+                
+            })
+
+}
+
 export const addProperty = (req: Request, res: Response) => {
     let {body, query} = req;
 
--- src/routes/property.ts
+++ src/routes/property.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { addProperty, getProperties, editProperty, deleteProperty, getSortedProperties } from '../controllers/property';
+import { addProperty, getProperties, editProperty, deleteProperty, getSortedProperties, getPropertyById } from '../controllers/property';
 import { verifyToken, verifyRole } from '../middlewares/authentication';
 
 export const property_router: Router = Router();
@@ -8,3 +8,4 @@ property_router.get('/properties', getProperties);
 property_router.put('/property/:id', verifyToken, verifyRole, editProperty);
 property_router.delete('/property/:id', verifyToken, verifyRole, deleteProperty);
 property_router.get('/properties/price', getSortedProperties);
+property_router.get('/property/:id', verifyToken, verifyRole, getPropertyById);
